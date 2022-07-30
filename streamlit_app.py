c35627cadc99ee5f0b1b2d7d8738dc6a6494e296
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 18:21:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+import streamlit as st
+import pandas as pd
 
 st.title('Here is your Flipcard Game to learn Spanish')
 st.text('We gathered 2000 most used Spanish words and create a game for you.')
