e01fa7e8875d0b6163c63ce9f6abba302061a737
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 18:24:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import pandas as pd
+import time
 
 st.title('Here is your Flipcard Game to learn Spanish')
 st.text('We gathered 2000 most used Spanish words and create a game for you.')
+
+
+
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.1)
+     my_bar.progress(percent_complete + 1)
