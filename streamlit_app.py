3ae25a9a4115d173a612eb297c7dfc203f90988e
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 18:49:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,14 @@ st.text('We gathered 2000 most used Spanish words and create a game for you.')
 
 my_bar = st.progress(0)
 
+'''
 for percent_complete in range(100):
      time.sleep(0.1)
      my_bar.progress(percent_complete + 1)
 
 if percent_complete == 99:
      st.write('Loading Complete')
-
+'''
      
 #st.button(label, key=None, help=None, on_click=None, disabled=False)
 
