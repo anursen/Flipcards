9ce509e62d4fd396bd57170c841f91212c9043ae
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 18:26:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,6 @@ my_bar = st.progress(0)
 for percent_complete in range(100):
      time.sleep(0.1)
      my_bar.progress(percent_complete + 1)
+
+if my_bar.progress == 100:
+     st.write('Loading Complate')
