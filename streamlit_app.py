849510c9551e19415756b716ab8ccd35428f01aa
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 23:13:17 -0400

    aaaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,7 @@
 import streamlit as st
 import pandas as pd
 import time
-
-
-class FlipCard:
-
-  def __init__(self,level=100):
-    '''
-
-
-
-    '''
-    self.level = level
-    self.score = 0
-    import pandas as pd
-    import numpy as np
-    path = 'es-en-2000.csv' 
-    # path = input('Lütfen Soru Bankasının yolunu giriniz!') 
-    self.df = pd.read_csv(path)
-    self.df['act-psv'] = [int(i) for i in np.linspace(1,1,2000)]
-    #print(df)
-    self.set_level()
-
-
-  def set_level(self):
-
-    self.level_df = self.df.iloc[:self.level,:]
-    #print(self.level_df)
-
-  def get_question(self):
-    try:
-      self.question_df = self.level_df[self.level_df['act-psv'] != 0].sample(n=1)
-      #self.question_index = self.question_df.index
-      return self.question_df.es.values[0]
-    except ValueError:
-      return 'False'
-
-
-
-  def get_answer(self):
-      return self.question_df.en.values[0]
-  
-  def set_status(self):
-    self.level_df.at[self.question_df.index.values[0],'act-psv'] = 0
-    self.set_score()
-  
-  def set_score(self):
-    self.score = self.level_df[self.level_df['act-psv'] == 1]['act-psv'].count()
-
-
-
+from flipcard import Flipcard
 
 
 st.title('Here is your Flipcard Game to learn Spanish')
@@ -75,6 +27,8 @@ st.write("Outside the form")
 
 st.code('for i in range(8): foo()')
 
+
+'''
 while level and seconds != None:
 
     game = FlipCard(level = level)
@@ -82,7 +36,7 @@ while level and seconds != None:
     
 
 
-'''
+
 my_bar = st.progress(0)
 
 
@@ -92,7 +46,7 @@ for percent_complete in range(100):
 
 if percent_complete == 99:
      st.write('Loading Complete')
-'''
+
      
 #st.button(label, key=None, help=None, on_click=None, disabled=False)
 
@@ -118,5 +72,5 @@ for i in range(3):
 df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
 st.table(df)
 
-
+'''
 
