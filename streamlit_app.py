3e32a7a1a457402daa709f1fc94e376c3e1f6020
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 22:58:02 -0400

    qq

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,16 +19,71 @@ with st.form("my_form"):
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-        key1 = slider_val
-        key2 = slider_val
+        level = slider_val
+        seconds = slider_val
         st.write(key1,key2)
 
 st.write("Outside the form")
 
 
 
-while key1 and key2 != None:
-    st.write('Lets go')
+class FlipCard:
+
+  def __init__(self,level=100):
+    '''
+
+
+
+    '''
+    self.level = level
+    self.score = 0
+    import pandas as pd
+    import numpy as np
+    path = 'es-en-2000.csv' 
+    # path = input('Lütfen Soru Bankasının yolunu giriniz!') 
+    self.df = pd.read_csv(path)
+    self.df['act-psv'] = [int(i) for i in np.linspace(1,1,2000)]
+    #print(df)
+    self.set_level()
+
+
+  def set_level(self):
+
+    self.level_df = self.df.iloc[:self.level,:]
+    #print(self.level_df)
+
+  def get_question(self):
+    try:
+      self.question_df = self.level_df[self.level_df['act-psv'] != 0].sample(n=1)
+      #self.question_index = self.question_df.index
+      return self.question_df.es.values[0]
+    except ValueError:
+      return 'False'
+
+
+
+  def get_answer(self):
+      return self.question_df.en.values[0]
+  
+  def set_status(self):
+    self.level_df.at[self.question_df.index.values[0],'act-psv'] = 0
+    self.set_score()
+  
+  def set_score(self):
+    self.score = self.level_df[self.level_df['act-psv'] == 1]['act-psv'].count()
+
+
+
+
+
+while level and seconds != None:
+
+    game = FlipCard(level = level)
+    question = game.get_question()
+    
+
+    
+    
 
 
 
