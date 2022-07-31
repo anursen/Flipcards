01de01e3534fd13f16f1906ac273acfa6db036aa
anursen <96952461+anursen@users.noreply.github.com>
2022-07-30 22:27:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ for i in range(3):
 df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
 st.table(df)
 
-st.form_submit_button(label="Submit", help=None)
+st.form_submit_button(label="Submit",on_click=None)
 
 
